6f17872d821b82333ba56f37663f8a3c5e0550be
Daudré-Vignier Charles <daudre.vignier.charles@mail.ru>
2018-03-27 17:05:39 +0200

    zz.main.propage now sync plugins folder and does not sync plugins/ressources/*

--- zsh/ressources/sources/main_propage.s.zsh
+++ zsh/ressources/sources/main_propage.s.zsh
@@ -4,7 +4,7 @@ zz.main.propage () {
 	rsync -v $ZDEV_PATH/zshrc.zsh $HOME/.zshrc
 	rsync -v $ZDEV_PATH/zshenv.zsh $HOME/.zshenv
 	rsync -vr --delete-after --exclude-from=/dev/stdin $ZDEV_PATH/zsh/ $HOME/.zsh <<EOF
-ressources/plugins/
+ressources/plugins/ressources/*
 TODO
 COPYING
 INSTALL
