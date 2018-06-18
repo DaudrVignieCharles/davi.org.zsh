e4f3b97db8aff6b7811560f05ef02ff9b91f4ab4
Daudré-Vignier Charles <daudre.vignier.charles@narod.ru>
2018-06-18 22:44:05 +0200

    update sudo_overwrite for root compatibility
    + remove unused files

--- zsh/ressources/alias/sudo_overwrite.s.zsh
+++ zsh/ressources/alias/sudo_overwrite.s.zsh
@@ -1,10 +1,12 @@
 #!/usr/bin/zsh
 
-() {
-    alias lidctl='sudo lidctl'
-    alias lumin='sudo lumin'
-    alias iftop='sudo iftop_choice'
-    alias logspynow='sudo logspynow'
-    alias fail2ban-log='sudo fail2ban-log'
-    alias dpkg='sudo dpkg'
-}
+if [[ $(id --user) -ne 0 ]] ; then
+    () {
+        alias lidctl='sudo lidctl'
+        alias lumin='sudo lumin'
+        alias iftop='sudo iftop_choice'
+        alias logspynow='sudo logspynow'
+        alias fail2ban-log='sudo fail2ban-log'
+        alias dpkg='sudo dpkg'
+    }
+fi
--- zsh/ressources/functions/sudo_overwrite.s.zsh
+++ zsh/ressources/functions/sudo_overwrite.s.zsh
@@ -1,12 +1,15 @@
 #!/usr/bin/zsh
-apt(){
-    (( $# == 0 )) && { /usr/bin/apt ; return 0 }
-    typeset -a suaptcmd
-    suaptcmd=(install update upgrade remove purge autoremove
-        full-upgrade dist-upgrade edit-sources autoclean)
-    if [[ -n ${suaptcmd[(r)$1]} ]]; then
-        sudo /usr/bin/apt "$@"
-    else
-        /usr/bin/apt "$@"
-    fi
-}
+
+if [[ $(id --user) -ne 0 ]] ; then
+    apt(){
+        (( $# == 0 )) && { /usr/bin/apt ; return 0 }
+        typeset -a suaptcmd
+        suaptcmd=(install update upgrade remove purge autoremove
+            full-upgrade dist-upgrade edit-sources autoclean)
+        if [[ -n ${suaptcmd[(r)$1]} ]]; then
+            sudo /usr/bin/apt "$@"
+        else
+            /usr/bin/apt "$@"
+        fi
+    }
+fi
